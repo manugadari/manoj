0c3240a313105987efa8159430ddaecdfbd2b29a
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-04-07 13:35:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-west-2"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
 }
