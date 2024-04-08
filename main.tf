3f85c117186000760f87851ac1a58e3b62be1469
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-04-08 12:06:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 
 provider "aws" {
   region  = "us-west-2"
@@ -7,4 +17,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
+  tags = {
+    Name = "Terraform_Demo"
+  }
 }
